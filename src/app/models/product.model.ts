bf6989268ae2143fb0b333f8ea230a91aebb760a
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-04 09:12:42 -0700

    add constructor for product model

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,6 @@
+export class Product{
+  public manufacturer: string = "Roland";
+  constructor(public type: string, public subtype: string, public model: string, public msrp: number, public title: string, public description: string, public image: string){}
+
+
+}
