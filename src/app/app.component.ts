32eba882dae186cb032e66b732545ef14112935d
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-04 09:28:32 -0700

    add master product array, currently with only 1 item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  masterProductArray: Product[]=[
+    new Product("Piano", "Digital Piano", "FP-90", 999, "Our Flagship Digital Piano", "lorehahs;ofdgh a;sfugh asiuf goasuaisugh asf giaufshg ialfh g", "https://static.roland.com/assets/images/products/categories/rct_pianos.jpg" )
+
+  ];
 }
