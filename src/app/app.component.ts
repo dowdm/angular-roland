88fa4de2a2c191f6e7c37c5fef89050b16c2d202
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-04 10:25:26 -0700

    add category constructor and begin to implement category instantiation in root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from './models/product.model';
-
+import { Category } from './models/category.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +10,10 @@ export class AppComponent {
 
   masterProductArray: Product[]=[
     new Product("Piano", "Digital Piano", "FP-90", 999, "Our Flagship Digital Piano", "lorehahs;ofdgh a;sfugh asiuf goasuaisugh asf giaufshg ialfh g", "https://static.roland.com/assets/images/products/categories/rct_pianos.jpg" )
+  ];
+
+  buttonCategory: Category[] = [
+    new Category("Products",["Pianos", "Organs", "Synthesizers", "Keyboards", "Guitar and Bass", "Drums and Percussion", "Production", "Amplifiers", "AIRA and DJ", "Pro Audio", "Pro Video", "Accessories"])
 
   ];
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+
 
 }
