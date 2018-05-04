ede05fd7b2fc8cc8f96aa5d9ccb8fb564c131be1
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-04 13:11:22 -0700

    try installing ng bootstrap, js functionality of bootstrap still missing. will bypass for now. adds loop for main carousel display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { Product } from './models/product.model';
 import { Category } from './models/category.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
