102f75710bfdeece35c5b1c903ef588dd3286774
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-10 16:08:30 -0700

    starts routing implemetation and refactoring of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MainCarouselComponent } from './main-carousel/main-carousel.component';
@@ -12,6 +12,14 @@ import { ProductCategoriesComponent } from './product-categories/product-categor
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { ProductsComponent } from './products/products.component';
+import { CommunityComponent } from './community/community.component';
+import { SupportComponent } from './support/support.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { BlogComponent } from './blog/blog.component';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -28,13 +36,21 @@ export const firebaseConfig = {
     FeaturedProductComponent,
     FeaturedVideoComponent,
     WhatsNewComponent,
-    ProductCategoriesComponent
+    ProductCategoriesComponent,
+    ProductsComponent,
+    CommunityComponent,
+    SupportComponent,
+    ArtistsComponent,
+    BlogComponent,
+    NewsComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     // FormsModule,
     // HttpModule,
-    // routing,
+    routing,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule
   ],
---
+++ src/app/app.routing.ts
@@ -0,0 +1,47 @@
+import { ModuleWithProviders }  from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CommunityComponent } from './community/community.component';
+import { SupportComponent } from './support/support.component';
+import { ArtistsComponent } from './artists/artists.component';
+import { BlogComponent } from './blog/blog.component';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path: 'community',
+    component: CommunityComponent
+  },
+  {
+    path: 'support',
+    component: SupportComponent
+  },
+  {
+    path: 'artists',
+    component: ArtistsComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
