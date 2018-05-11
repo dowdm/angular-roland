900421fc2b276b5816b8c31eec7d4934ca25a668
Matt Dowdney <theofficialmatt@gmail.com>
2018-05-11 14:37:41 -0700

    add dummy product registration after user login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+ register=false;
 
+ regProduct(){
+   this.register=true;
+   return this.register;
+ }
+
+ submitRegister(){
+   this.register=false;
+ }
   constructor() { }
 
   ngOnInit() {
